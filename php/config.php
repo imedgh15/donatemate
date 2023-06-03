5ee12db5343b7e23332a7d4d93861b6fb398c8bb
imedgh15 <135194313+imedgh15@users.noreply.github.com>
2023-06-03 18:21:48 +0100

    Update config.php

--- php/config.php
+++ php/config.php
@@ -1,12 +1,15 @@
 <?php
+session_start();
 $servername = "localhost";
 $username = "root";
 $password = "";
 $dbname = "donormate";
+$path = dirname(__FILE__);
+$pieces = explode(DIRECTORY_SEPARATOR, $path);
+$BASEDIR= $pieces[count($pieces) - 2];
 
-// Création de la connexion
 $conn = mysqli_connect($servername, $username, $password, $dbname);
-
+require_once "functions.php";
 // Vérification de la connexion
 if ($conn->connect_error) {
     die("La connexion a échoué: " . $conn->connect_error);
